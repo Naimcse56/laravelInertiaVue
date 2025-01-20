91eca286409a4e74106c808d510479263bd378e0
MD NAIMUL ISLAM <naimcse56@gmail.com>
2025-01-20 17:16:00 +0600

    make component textInput

---
+++ resources/js/Components/TextInput.vue
@@ -0,0 +1,29 @@
+<script setup>
+const model = defineModel({
+    type: null,
+    required: true,
+});
+
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: 'text',
+    },
+    message: String
+})
+</script>
+<template>
+    <div class="mb-4">
+        <label :for="name" class="mb-1">{{label}}</label>
+        <input :type="type" :id="name" v-model="model" :class="{'!ring-red-500' : message}">
+        <small class="error" v-if="message">{{ message }}</small>
+    </div>
+</template>
--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive } from 'vue';
 import { useForm  } from '@inertiajs/vue3'
+import TextInput from '../Components/TextInput.vue';
 
 const form = useForm({
     name: null,
@@ -21,26 +22,10 @@ const submit = () => {
     <h1 class="title">Register A New Account</h1>
     <div class="w-2/4 mx-auto">
         <form @submit.prevent="submit">
-            <div class="mb-6">
-                <label for="name">Name</label>
-                <input type="text" id="name" v-model="form.name">
-                <small v-if="form.errors.name">{{ form.errors.name }}</small>
-            </div>
-            <div class="mb-6">
-                <label for="email">Email</label>
-                <input type="email" id="email" v-model="form.email">
-                <small v-if="form.errors.email">{{ form.errors.email }}</small>
-            </div>
-            <div class="mb-6">
-                <label for="password">Password</label>
-                <input type="password" id="password" v-model="form.password">
-                <small v-if="form.errors.password">{{ form.errors.password }}</small>
-            </div>
-            <div class="mb-6">
-                <label for="password_confirmation">Confirm Password</label>
-                <input type="text" id="password_confirmation" v-model="form.password_confirmation">
-                <small v-if="form.errors.password_confirmation">{{ form.errors.password_confirmation }}</small>
-            </div>
+            <TextInput name="name" label="Name" v-model="form.name" :message="form.errors.name"/>
+            <TextInput type="email" name="email" label="Email" v-model="form.email" :message="form.errors.email"/>
+            <TextInput type="password" name="password" label="Password" v-model="form.password" :message="form.errors.password"/>
+            <TextInput type="password" name="password_confirmation" label="Confirm Password" v-model="form.password_confirmation" :message="form.errors.password_confirmation"/>
             <div>
                 <p class="text-slate-600 mb-2">Already a user? <a href="#" class="text-link">Login</a></p>
                 <button class="primary-btn" :disabled="form.processing">Register Now</button>
