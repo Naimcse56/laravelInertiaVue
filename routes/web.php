68b39e8998543d38c4380bd8d4273ff6637a2980
MD NAIMUL ISLAM <naimcse56@gmail.com>
2025-01-20 18:50:48 +0600

    logout

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,10 @@ use App\Http\Controllers\AuthController;
 // });
 
 Route::inertia('/', 'Home')->name('home');
-Route::inertia('/register', 'Auth/Register')->name('register');
-Route::post('/register',[AuthController::class, 'register'])->name('register');
-Route::inertia('/login', 'Auth/Login')->name('login');
-Route::post('/login',[AuthController::class, 'login'])->name('login');
+Route::middleware(['guest'])->group(function () {
+    Route::inertia('/register', 'Auth/Register')->name('register');
+    Route::post('/register',[AuthController::class, 'register'])->name('register');
+    Route::inertia('/login', 'Auth/Login')->name('login');
+    Route::post('/login',[AuthController::class, 'login'])->name('login');
+});
 Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
