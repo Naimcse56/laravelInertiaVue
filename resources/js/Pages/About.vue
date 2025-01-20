132f476e2fc8234a5b1d0fbebf15119016556088
MD NAIMUL ISLAM <naimcse56@gmail.com>
2025-01-20 11:23:37 +0600

    route

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps({
+        user: String
+    })
+</script>
+
+<template>
+    <div>
+        <h1>Hello Buddy ! This is {{ user }}'s' Page</h1>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+// Route::get('/about', function () {
+//     return inertia('About', ['user' => 'Deamon']);
+// });
+
+Route::inertia('/', 'Home');
+Route::inertia('/about', 'About', ['user' => 'Deamon']);
